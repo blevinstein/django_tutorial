620794ccdfa7896fc233e397a96ff7f0facfc2a7
Brian Levinstein <blevinstein@gmail.com>
2013-06-25 01:05:55 -0700

    Basic index and detail implementation.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = { 'latest_poll_list': latest_poll_list }
+    return render(request, 'index.html', context)
+
+def detail(request, poll_id):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
